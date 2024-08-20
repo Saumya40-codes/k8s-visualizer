8dbd3166df18044c01b08d6c93d4f027a7325887
= <saumyabshah90@gmail.com>
2024-08-20 20:29:13 +0530

    start dev server on monitoring

--- main.go
+++ main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"fmt"
+  "os/exec"
 
 	"github.com/Saumya40-codes/k8s-namespace-visualizer/api"
 )
 
 func main() {
 	fmt.Println("🚀 Starting monitoring your k8s")
-	api.StartMonitoring()
+	go api.StartMonitoring()
+
+  cmd := exec.Command("npm", "run", "dev")
+	cmd.Dir = "./ui"
+
+	err := cmd.Start()
+	if err != nil {
+		fmt.Printf("Error starting Visualizer app: %v\n", err)
+		return
+	}
+
+  fmt.Println("😁 Visualizer started successfully!\nHeadover to http://localhost:5173")
+	select {}
 }
