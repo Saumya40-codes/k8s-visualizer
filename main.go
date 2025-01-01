786f93e5d90f6188f002659b0ff2c4536e85b595
= <saumyabshah90@gmail.com>
2025-01-01 16:21:21 +0530

    Shift logging to stdout/stderr only
    Signed-off-by: = <saumyabshah90@gmail.com>

--- main.go
+++ main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"log"
-	"os"
 
 	"github.com/Saumya40-codes/k8s-visualizer/api"
 )
 
 func init() {
-	logFile, _ := os.OpenFile("k8s-visualize.log", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
-	log.SetOutput(logFile)
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 }
 
 func main() {
